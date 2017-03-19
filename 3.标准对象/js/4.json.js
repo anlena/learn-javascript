2eef11d1963fd41f15665efa3119118a1cacbd0d
anlena <super_ironman@163.com>
2017-03-19 23:32:45 +0800

    hello

---
+++ 3.标准对象/js/4.json.js
@@ -0,0 +1,33 @@
+function success(text) {
+    var textarea = document.getElementById('test-response-text');
+    // textarea.value = text;
+    console.log(text);
+}
+
+function fail(code) {
+    var textarea = document.getElementById('test-response-text');
+    textarea.value = 'Error code: ' + code;
+}
+
+var request = new XMLHttpRequest(); // 新建XMLHttpRequest对象
+
+request.onreadystatechange = function () { // 状态发生变化时，函数被回调
+    if (request.readyState === 4) { // 成功完成
+        // 判断响应结果:
+        if (request.status === 200) {
+            // 成功，通过responseText拿到响应的文本:
+            return success(request.responseText);
+        } else {
+            // 失败，根据响应码判断失败原因:
+            return fail(request.status);
+        }
+    } else {
+        // HTTP请求还在继续...
+    }
+}
+
+// 发送请求:
+request.open('GET', 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json');
+request.send();
+
+alert('请求已发送，请等待响应...');
